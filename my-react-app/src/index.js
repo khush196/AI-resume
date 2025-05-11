75b04f6c2480116cd940e4a49a66b2cc223fdf9d
khush196 <khushpaliwal2005@gmail.com>
2025-05-11 15:35:25 +0530

    Update index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Import global styles
-import App from './App.jsx'; // Updated extension to .jsx
+import './index.css'; 
+import App from './App.jsx'; 
 import { ResumeProvider } from './contexts/ResumeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
